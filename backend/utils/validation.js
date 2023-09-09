245ec4cc225ff24aa8a84f94378afc8f9a095581
ApostalDante <apostal666666@gmail.com>
2023-09-09 22:42:16 +0300

    hotfix/review

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -1,6 +1,6 @@
 const { celebrate, Joi } = require('celebrate');
 
-const regex = /^(https?:\/\/)?[^\s]*\.(jpeg|png|ico|gif|webp|bmp|test)$/;
+const regex = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}(?:jpeg|png|ico|gif|webp|bmp|test)|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}(?:jpeg|png|ico|gif|webp|bmp|test)|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}(?:jpeg|png|ico|gif|webp|bmp|test)|www\.[a-zA-Z0-9]+\.[^\s]{2,}(?:jpeg|png|ico|gif|webp|bmp|test))/;
 
 const validateCreateUser = celebrate({
   body: Joi.object().keys({
